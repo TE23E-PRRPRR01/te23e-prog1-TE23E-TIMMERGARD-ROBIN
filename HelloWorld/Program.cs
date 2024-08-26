2a90f59eb7f3d13d688119439c62b6f711da21e4
Robin Timmergård. <robin.timmergard@elev.ga.ntig.se>
2024-08-26 14:32:23 +0200

    Tränat på cw, cr och svenskatäcken

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+Console.Clear();
+Console.WriteLine("Hej världen!");
+Console.WriteLine("Bananer");
+Console.Write("Vad heter du?");
+string namn = Console.ReadLine();
+Console.WriteLine("Hej"+ namn);
+
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("Vilken gren tittade du på OS?");
+Console.WriteLine("Vilken gillar du mest?");
