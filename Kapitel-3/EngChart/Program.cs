0c09973b6ec8d22fbbda86a50d0182e713a8fd8b
Robin Timmergård. <robin.timmergard@elev.ga.ntig.se>
2024-09-02 14:51:23 +0200

    Tränat if-satser

---
+++ Kapitel-3/EngChart/Program.cs
@@ -0,0 +1,60 @@
+﻿// Program som simulerar kända Engineering Flowchart
+Console.Clear();
+Console.WriteLine("Välkommen till ingenjörens flödesschema");
+
+// Visa en ASCii art
+Console.WriteLine($"""
+                       -   ,--_--.
+               -           \      `.
+                      -     "-_ _   \
+     -                         / F   )
+                   -     -    / / `--'
+              -              / /
+                   -        / /
+            -            __/ /
+                        /,-pJ
+           -        _--"-L ||
+                  ,"      "//
+     -           /  ,-"-".//\
+                /  (     // J____
+               J  (     // L/----\
+   .           F (     //__//^---'
+     `     ___J  F    '----| |
+  `       J---|  F         ) F
+    `. `   `--J  L        )  F
+    .   .`     L J       )  F
+       .  .    J  \    ,"  F
+         .  `.` \  "--"  ,"
+    hs      ` ``."-____-"
+ """);
+
+ Console.Write("Does it move?(Y/N) ");
+ string answer = Console.ReadLine();
+ 
+ // Kolla om svar är ja eller nej
+ if (answer == "Y")
+ { // Y = Yes
+    Console.Write("Should it? (Y/N) ");
+    answer = Console.ReadLine();
+    if (answer == "Y")
+    {
+        Console.WriteLine("No problem!");
+    }
+    else
+    {
+        Console.WriteLine("Please use Duct Tape!");
+    }
+ }
+ else
+ { // N = No
+    Console.Write("Should it? (Y/N) ");
+    answer = Console.ReadLine();
+    if (answer == "Y")
+    {
+        Console.WriteLine("Please use WD-40!");
+    }
+    else
+    {
+        Console.WriteLine("No Problem!");
+    }
+ }
---
+++ Kapitel-3/InteractivStory/Program.cs
@@ -0,0 +1,33 @@
+﻿// Program för en interaktiv historia
+Console.Clear();
+Console.Write("Vill du ha en korv? (Ja/Nej) ");
+string answer = Console.ReadLine();
+
+//Kolla om svar är Ja eller Nej/
+if (answer == "Ja")
+{ // Ja = Yes
+    Console.Write("Vilken sort? (Vanlig/Kyckling) ");
+    Console.ReadLine();
+
+    if (answer == "Ja")
+    {
+        Console.WriteLine("Varsågod!");
+    }
+    else
+    {
+        Console.WriteLine("Köp en då!");
+    }
+}
+else
+{ // Nej = No
+    Console.Write("Är du säker? (Ja/Nej) ");
+    answer = Console.ReadLine();
+    if (answer == "Ja")
+    {
+        Console.WriteLine("Ha en fortsatt trevlig dag!");
+    }
+    else
+    {
+        Console.WriteLine("Då får du tänka igen, ha en bra dag!");
+    }
+}
