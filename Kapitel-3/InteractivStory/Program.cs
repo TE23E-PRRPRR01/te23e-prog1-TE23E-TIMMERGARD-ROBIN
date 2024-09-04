38452fc382bce851b53009dd55996fc3d4f741dc
Robin Timmergård. <robin.timmergard@elev.ga.ntig.se>
2024-09-04 14:57:21 +0200

    fortsätt på if-else

--- Kapitel-3/InteractivStory/Program.cs
+++ Kapitel-3/InteractivStory/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Program för en interaktiv historia
 Console.Clear();
 Console.Write("Vill du ha en korv? (Ja/Nej) ");
-string answer = Console.ReadLine();
+string answer = Console.ReadLine().ToLower();//.ToUpper()
 
 //Kolla om svar är Ja eller Nej/
 if (answer == "Ja")
 { // Ja = Yes
     Console.Write("Vilken sort? (Vanlig/Kyckling) ");
-    Console.ReadLine();
+    Console.ReadLine().ToLower();//.ToUpper()
 
     if (answer == "Ja")
     {
@@ -21,7 +21,7 @@ if (answer == "Ja")
 else
 { // Nej = No
     Console.Write("Är du säker? (Ja/Nej) ");
-    answer = Console.ReadLine();
+    answer = Console.ReadLine().ToLower();//.ToUpper()
     if (answer == "Ja")
     {
         Console.WriteLine("Ha en fortsatt trevlig dag!");
@@ -31,3 +31,4 @@ else
         Console.WriteLine("Då får du tänka igen, ha en bra dag!");
     }
 }
+
