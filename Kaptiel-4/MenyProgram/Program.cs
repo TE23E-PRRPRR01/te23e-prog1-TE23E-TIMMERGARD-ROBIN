a33d39af9834977d1f4bef081eff0f129ab345d2
Robin Timmergård. <robin.timmergard@elev.ga.ntig.se>
2024-09-23 14:47:09 +0200

    Tränat på menyer och loopar

---
+++ Kaptiel-4/MenyProgram/Program.cs
@@ -0,0 +1,46 @@
+﻿// Litet program med meny
+Console.Clear();
+Console.WriteLine("Litet program med meny");
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+// Variabel för valet
+string val = "";
+
+// Programloop som snurrar tills användaren säger stopp
+while (true)
+{
+    // Visar en meny och läser användarens val
+    Console.Write("""
+    1. Omvandla en text till versalen
+    2. Omvandla en text till gemener
+    3. Avsluta 
+    Välj ett av alternativen ovan: 
+    """);
+    val = Console.ReadLine();
+    
+    // Hantera användarens val
+
+    if (val == "1")
+    {
+        Console.Write("Skriv in en text: ");
+        string texten = Console.ReadLine().ToUpper();
+        Console.WriteLine($"Texten i versaler blir: {texten}");
+    }
+    else if (val == "2")
+    {
+        Console.Write("Skriv in en text: ");
+        string texten = Console.ReadLine().ToLower();
+        Console.WriteLine($"Texten i versaler blir: {texten}");
+        
+    }
+     else if (val == "3")
+    {
+        Console.WriteLine("Avslutar...");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Du valde ogiltit alternativ, försök igen");
+    }
+}
---
+++ Kaptiel-4/TextEditor/Program.cs
@@ -0,0 +1,59 @@
+﻿// Program som skriva/läsa textfil
+Console.Clear();
+Console.WriteLine("Program som skriver/läser textfil");
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+string val = "";
+
+while (true)
+{
+    Console.Clear();
+    Console.Write($"""
+    Välkommen till TextEditor
+    1. Skriver till fil
+    2. Läser från fil
+    3. Avslutar programmet
+    Välj ett av alternativen ovan: 
+    """);
+    val = Console.ReadLine();
+    
+    if (val == "1")
+  {
+    Console.Clear();
+    Console.Write("Skriv en text: ");
+    string texten = Console.ReadLine();
+    Console.Write("Vad ska filen heta?: ");
+    string namn = Console.ReadLine();
+     File.WriteAllText($"{namn}.txt", texten);
+  }
+  else if (val == "2")
+  {
+
+    try
+    {
+        Console.Clear();
+        Console.Write("Namn på filen: ");
+        string namn = Console.ReadLine();
+        string texten = File.ReadAllText($"{namn}.txt");
+        Console.Write($"{texten}"); 
+    }
+    catch (Exception ex)
+    {
+        Console.Clear();
+    }
+  }
+  else if (val == "3")
+  {     
+    Console.Clear();
+    Console.WriteLine("Hejdå!");
+    break;
+    Console.Clear();
+  }
+  else
+  {
+    Console.WriteLine("Ogiltigt alternativ, försök igen");
+  }
+
+
+}
