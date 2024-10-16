de69c126a427b4d6f92aa49617a80a108abaf87e
Robin Timmergård. <robin.timmergard@elev.ga.ntig.se>
2024-10-16 15:00:07 +0200

    tränat på list, index, foreach-loop

---
+++ Kaptiel-5/Listor/Program.cs
@@ -0,0 +1,50 @@
+﻿// Använda listor för att skapa/hantera samlingar
+Console.Clear();
+
+// Lista på trevliga frukter, tex "äpple", "apelsin", "ananas"
+List<string> frukter = ["äpple", "apelsin", "ananas"];
+
+Console.WriteLine($"Min fruktlista: {frukter[0]}, {frukter[1]}, {frukter[2]}");
+
+// Ändra i listan:
+frukter[0] = "banan";
+Console.WriteLine($"Min fruktlista: {frukter[0]}, {frukter[1]}, {frukter[2]}");
+
+// Loopa igenom listan = loopa igenom = från första till sista
+foreach (var frukt in frukter)
+{
+    Console.WriteLine(frukt);
+}
+
+// Skapa en lista på valfri ska 4st
+// Skriv ut varje sak på samma rad
+List<string> städer = ["Stockholm", "Borås", "Göteborg"];
+// Console.WriteLine($"Mina städer: {städer[0]}, {städer[1]}, {städer[2]}");
+foreach (var stad in städer)
+{
+    Console.Write($"stad");
+}
+for (int i = 0; i < städer.Count; i++)
+{
+    Console.Write($"{städer[i]}");
+}
+
+// Skapa ett register
+Console.Clear();
+// En tom lista
+List<string> namnlista = [];
+
+// While-loop
+while (true)
+{
+    Console.Write("Ange ett namn: ");
+    string namn = Console.ReadLine();
+    namnlista.Add(namn);
+
+    // Skriv ut hela listan på en rad
+    foreach (var namnet in namnlista)
+    {
+        Console.Write($"{namnet}, ");
+    }
+    Console.WriteLine();
+}
---
+++ Kaptiel-5/Morsekod/Program.cs
@@ -0,0 +1,65 @@
+﻿// En liten morsekod app
+Console.Clear();
+Console.WriteLine("En liten morsekod app");
+Console.InputEncoding = System.Text.Encoding.Unicode;
+
+// Två listor
+List<string> alfabetet = [ "A", "B", "C", "D", "E", "F", "G", "H", "I",
+                           "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+                           "S", "T", "U", "V", "W", "X", "Y", "Z", "Å",
+                           "Ä", "Ö", " ", "1", "2", "3", "4", "5",
+                           "6", "7", "8", "9", "0"                       ];
+List<string> morsekod = [ ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                          "....", "..", ".---", "-.-", ".-..", "--", "-.", 
+                          "---", ".--.","--.-", ".-.", "...", "-", "..-", 
+                          "...-", ".--", "-..-", "-.--", "--..", ".--.-", 
+                          ".-.-", "---.", "/", ".---","..---", "...--", "....-", 
+                          ".....", "-....", "--...", "---..", "----.", "-----"];
+
+// Läs in en text
+Console.Write("Ange ett meddelande: ");
+string meddelande = Console.ReadLine();
+
+// Gå igenom meddelandet bokstav för bokstav (loop)
+foreach (char bokstav in meddelande)
+{
+    // Uppslag i alfabetet efter index
+    int index = alfabetet.IndexOf(bokstav.ToString());
+
+    // Hittar morsetecken?
+    if (index >= 0)
+    {
+        // Console.WriteLine($"{bokstav} finns på index {index}");
+
+        // Plocka ut morsetecknet för detta index
+        string morsetecken = morsekod[index];
+        // Console.WriteLine($"{bokstav} är {morsetecken}");
+        Console.Write($"{morsetecken} ");
+
+        // Spela upp morse som ljud-beep
+        // Tex D = "-.."
+        // Dvs loopa igenom morsetecknet
+        foreach (char signal in morsetecken)
+        {
+                if (signal == '.') // '.
+                {
+                    // 1000Hz, 200ms
+                    Console.Beep(1000, 200);
+                }
+                else // '-'
+                {
+                    Console.Beep(1000, 600);
+                }
+        }
+        
+        // Paus i koden
+        Thread.Sleep(150);
+    }
+    else
+    {
+        Console.WriteLine("? ");
+    }
+
+
+}
+
