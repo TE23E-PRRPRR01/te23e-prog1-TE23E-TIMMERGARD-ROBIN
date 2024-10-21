64404b96e1d74e06a59f22a4c8186043a5361d7e
Robin Timmergård. <robin.timmergard@elev.ga.ntig.se>
2024-10-21 14:48:35 +0200

    CaesarKrypto

---
+++ Kaptiel-5/CeasarKrypto/Program.cs
@@ -0,0 +1,41 @@
+﻿// Ett program som för kryptering med Caesar-chiffer
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Ett program som för kryptering med Caesar-chiffer");
+
+// Alfabetet, lista av bokstäver att använda
+string alfabetet = "ABCDEFGHJIKLMNOPQRSTUVWXYZÅÄÖ";
+
+// Ange en bokstav
+Console.Write("Ange ett meddelande: ");
+string meddelande = Console.ReadLine().ToUpper();
+
+// Ange en nyckel (1-9)
+Console.Write("Ange en nyckel (1-9): ");
+int nyckel = int.Parse(Console.ReadLine());
+
+foreach (var bokstav in meddelande)
+{
+// Hitta bokstavens position (index)
+int index = alfabetet.IndexOf(bokstav);
+Console.WriteLine($"'A' så är index={index}");
+
+// Om bokstaven finns i alfabetet
+if (index != -1)
+{
+// Caesar-kryptering, addera en nyckel (tex, 2)
+int nyIndex = index + nyckel;
+Console.WriteLine($"{index} + {nyckel} = {nyIndex}");
+
+// Plocka ut bokstaven för nyIndex
+char krypteradBokstav = alfabetet[nyIndex];
+Console.WriteLine($"{nyIndex} är bokstaven {krypteradBokstav}");
+    
+}
+else
+{
+    
+}
+    
+}
+
