de60afb647089f4812909d009227c189a3a1a307
Robin Timmergård. <robin.timmergard@elev.ga.ntig.se>
2024-11-13 15:01:00 +0100

    Tränat på if-satser, loopar, list

---
+++ Kaptiel-5/SlumpadLista/Program.cs
@@ -0,0 +1,30 @@
+﻿// Slumpa många tal och spara i lista
+Console.Clear();
+Console.WriteLine("Slumpa många tal och spara i lista");
+
+// Globala variabler
+// En lista för heltal
+List<int> listaSlumptal = [];
+
+// Be användaren ange antal slumpade tal
+Console.Write("Hur många tal vill du slumpa?: ");
+int antal = int.Parse(Console.ReadLine());
+
+// Be användaren ange min & max slumptal
+Console.Write("Ange minsta  värde slumptal: ");
+int min = int.Parse(Console.ReadLine());
+Console.Write("Ange max värde slumptal: ");
+int max = int.Parse(Console.ReadLine());
+
+// Loopa 5 ggr
+for (int i = 0; i < antal; i++)
+{
+    // Slumpa ett tal 1-100
+    int slumptal = 0;
+    
+    // Lägg till slumptalet i listan
+    listaSlumptal.Add(slumptal);
+
+    slumptal = Random.Shared.Next(max, max+1);
+    Console.WriteLine($"Slumpat tal {i + 1}: {slumptal}");
+}
---
+++ Kaptiel-5/SlumpadLista2/Program.cs
@@ -0,0 +1,72 @@
+﻿// Skapa lista med slumpade tal
+// Validera användarens inmatning
+Console.Clear();
+Console.WriteLine("");
+
+// Deklarera variabler
+// Skapa en lista för att spara det slumpade talet
+List<int> listaSlumptal = [];
+
+// Fråga användaren hur många slumptal som ska skapas
+Console.Write("Ange antal slumptal: ");
+int antal = 0;
+while (true)
+{
+
+    bool lyckades = int.TryParse(Console.ReadLine(), out antal);
+
+    if (lyckades)
+    {
+        Console.WriteLine($"Du vill ha {antal} slumptal!");
+    }
+    else
+    {
+        Console.Write("Du måste skriva ett heltal: ");
+    }
+}
+Console.WriteLine($"Du vill ha {antal} slumptal");
+
+// Användaren mata in min-värde
+int min = 0;
+while (true)
+{
+    // Fråga användaren hur många slumptal som skall skapas
+    Console.Write("Ange högsta slumptal: ");
+
+    bool lyckades = int.TryParse(Console.ReadLine(), out min);
+    if (lyckades)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Du måste skriva ett heltal");
+    }
+}
+Console.WriteLine($"Du vill ha {min} slumptal");
+
+// Användaren mata in max-värde
+int max = 0;
+while (true)
+{
+    // Fråga användaren hur många slumptal som skall skapas
+    Console.Write("Ange högsta slumptal: ");
+
+    bool lyckades = int.TryParse(Console.ReadLine(), out max);
+    if (lyckades)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Du måste skriva ett heltal");
+    }
+}
+Console.WriteLine($"Du vill ha {max} slumptal");
+
+// Slumpa fram ett annat tal
+for(int i = 0; i < antal; i++)
+{
+    int slumptal = Random.Shared.Next(min, max + 1);
+    Console.WriteLine($"Ditt nya slumptal är {slumptal}");
+}
