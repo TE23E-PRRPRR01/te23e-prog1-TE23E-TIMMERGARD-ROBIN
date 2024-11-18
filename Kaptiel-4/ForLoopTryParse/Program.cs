7130b17adbbc75673b223c6dfad3bf0c045b011b
Robin Timmergård. <robin.timmergard@elev.ga.ntig.se>
2024-11-18 14:43:46 +0100

    ForLoopTryParse

---
+++ Kaptiel-4/ForLoopTryParse/Program.cs
@@ -0,0 +1,39 @@
+﻿// Program som loopar städer flera gånger
+Console.Clear();
+Console.WriteLine("Program som loopar städer flera gånger");
+
+while (true)
+{
+
+    Console.Write("Ange en stad: ");
+    string stad = Console.ReadLine();
+
+    Console.Write("Ange hur många gånger du vill repetera din stad: ");
+
+    string antalText = Console.ReadLine();
+    int antal = 0;
+    bool success = int.TryParse(antalText, out antal);
+    if (true)
+    {
+
+        for (int i = 0; i < antal; i++)
+        {
+            Console.WriteLine($"{i}. {stad}");
+        }
+
+        // Be användaren mata in ytterligare ett tal
+        Console.Write("Ange ett ytterligare tal: ");
+        string talText = Console.ReadLine();
+        int tal = 0;
+        success = int.TryParse(talText, out tal);
+
+        // Skriv ut resultaten av följande beräkningar på talet¨
+        // Multiplicera det med 2.
+        Console.WriteLine($"Multiplicera med 2: {tal * 2}");
+        // Dividera det med 2.
+        Console.WriteLine($"Dividerat med 2: {(tal * 2)} / 2");
+        // Addera 2.
+        Console.WriteLine($"Addera med 2 {(tal * 2) / 2}) + 2");
+        // Subtrahera 2.
+    }
+}
---
+++ Kaptiel-5/Listor/Dungeon/Program.cs
@@ -0,0 +1,156 @@
+﻿// Ett dungeon-spel med massa rum, saker, fiender, skatter...
+Console.Clear();
+Console.WriteLine("Ett dungeon-spel!");
+
+// Programvariabler (Statevariables)
+string rum = "hallen";
+List<string> inventarie = [];
+
+// Spelloop
+while (true)
+{
+    // Är vi i hallen?
+    if (rum == "hallen")
+    {
+        Console.WriteLine("Du är i hallen");
+        Console.WriteLine("1. Titta runt");
+        Console.WriteLine("2. Gå till nästa rum");
+        Console.Write("Vad vill du göra?: ");
+        string val = Console.ReadLine();
+        if (val == "1")
+        {
+            Console.WriteLine("Du tittar runt i rummet och ser några gamla tavlor.");
+        }
+        else if (val == "2")
+        {
+            rum = "rum 1";
+            Console.WriteLine("... du går in i nästa rum...");
+        }
+    }
+    else if (rum == "rum 1")
+    {
+        Console.WriteLine("Du är i rum 1");
+        Console.WriteLine("1. Kolla runt");
+        Console.WriteLine("2. Gå tillbaka till hallen");
+        Console.WriteLine("3. Gå till nästa rum");
+        Console.Write("Vad vill du göra?: ");
+        string val = Console.ReadLine();
+        if (val == "1")
+        {
+            Console.WriteLine("Du ser en nyckel på golvet.");
+            Console.Write("Vill du plocka upp nyckel (j/n)");
+            val = Console.ReadLine().ToLower();
+            if (val == "j")
+            {
+                inventarie.Add("nyckel");
+                Console.WriteLine("Du plockade upp nyckeln.");
+            }
+            else
+            {
+                Console.WriteLine("Du lämnar nyckeln på golvet");
+            }
+        }
+        else if (val == "2")
+        {
+            rum = "hallen";
+            Console.WriteLine(" .. du går tillbaka till hallen.. ");
+        }
+        else if (val == "3")
+        {
+            rum = "rum 2";
+            Console.WriteLine(" .. du går in i nästa rum .. ");
+        }
+    }
+    else if (rum == "rum 2")
+    {
+        Console.WriteLine("Du är i rum 2");
+        Console.WriteLine("1. Titta runt");
+        Console.WriteLine("2. Gå tillbaka till rum 1");
+        Console.WriteLine("3. Gå till nästa rum");
+        Console.Write("Vad vill du göra? ");
+        string val = Console.ReadLine();
+        if (val == "1")
+        {
+            Console.WriteLine("Du ser en pizza på bordet");
+            Console.Write("Vill du äta den? (j/n): ");
+            val = Console.ReadLine().ToLower();
+            if (val == "j")
+            {
+                Console.WriteLine("Du åt pizzan och blev förgiftad!");
+                Console.WriteLine("Game over!");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Du lämnar pizzan på bordet.");
+                Console.Write("Vill du plocka upp pizzan? (j/n)");
+                val = Console.ReadLine().ToLower();
+                if (val == "j")
+                {
+                    inventarie.Add("Pizza");
+                    Console.WriteLine("Du plockade upp pizzan!");
+                }
+                else
+                {
+                    Console.WriteLine("Du lämnar pizzan på bordet!");
+                }
+            }
+        }
+        else if (val == "2")
+        {
+            rum = "rum 1";
+            Console.WriteLine(" .. du går tillbaka till rum 1.. ");
+        }
+        else if (val == "3")
+        {
+            rum = "rum 3";
+            Console.WriteLine(" .. du går in i nästa rum .. ");
+        }
+    }
+    else if (rum == "3")
+    {
+        Console.WriteLine("Du är i rum 3");
+        Console.WriteLine("1. Titta runt");
+        Console.WriteLine("2. Gå tillbaka till rum 2");
+        Console.Write("Vad vill du göra? ");
+        string val = Console.ReadLine();
+        if (val == "1")
+        {
+            Console.WriteLine("Du ser ett monster som sover");
+            Console.Write("Vill du väcka den? (j/n): ");
+            val = Console.ReadLine().ToLower();
+            if (val == "j")
+            {
+                Console.WriteLine("Du väckte monstret!");
+                Console.WriteLine("");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Du lämnar pizzan på bordet.");
+                Console.Write("Vill du plocka upp pizzan? (j/n)");
+                val = Console.ReadLine().ToLower();
+                if (val == "j")
+                {
+                    inventarie.Add("Pizza");
+                    Console.WriteLine("Du plockade upp pizzan!");
+                }
+                else
+                {
+                    Console.WriteLine("Du lämnar pizzan på bordet!");
+                }
+            }
+        }
+        else if (val == "2")
+        {
+            rum = "rum 1";
+            Console.WriteLine(" .. du går tillbaka till rum 1.. ");
+        }
+        else if (val == "3")
+        {
+            rum = "rum 3";
+            Console.WriteLine(" .. du går in i nästa rum .. ");
+        }
+    }
+
+}
