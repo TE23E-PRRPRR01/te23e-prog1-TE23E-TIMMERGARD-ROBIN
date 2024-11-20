fbf5802acc1f3b9109b4dc3af17917f9c6ade7e2
Robin Timmergård. <robin.timmergard@elev.ga.ntig.se>
2024-11-20 14:55:31 +0100

    ForBonanza

---
+++ Kaptiel-4/ForBonanza/Program.cs
@@ -0,0 +1,19 @@
+﻿// Ett bonanza spel
+Console.Clear();
+
+Console.Write("Ange startvärde: ");
+int startvärde = int.Parse(Console.ReadLine());
+
+Console.Write("Ange slutvärde: ");
+int slutvärde = int.Parse(Console.ReadLine());
+
+
+int summan = 0;
+
+for (var i = startvärde; i < slutvärde+1; i++)
+{
+    Console.WriteLine(i);
+    summan += i;
+}
+
+Console.WriteLine($"Summan av siffrorna {startvärde} till {slutvärde} är {summan}");
